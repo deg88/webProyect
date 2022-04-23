255aba20dd62f7667b4a252a740fcc7eb575ee05
degutierrezda <degutierrezda@poligran.edu.co>
2022-04-23 13:52:46 -0500

    connect data cluster

---
+++ fda/src/main/java/com/poli/fda/controller/CompuestosController.java
@@ -0,0 +1,36 @@
+package com.poli.fda.controller;
+
+import com.poli.fda.entity.Compuesto;
+import com.poli.fda.repository.CompuestoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController()
+@RequestMapping("/parcial")
+public class CompuestosController {
+
+    @Autowired
+    CompuestoRepository compuestoRepository;
+
+    //Load List of Books
+    @PostMapping("/compuestos")
+    public String createEmployeeList(@RequestBody List<Compuesto> compuestos) {
+        compuestoRepository.saveAll(compuestos);
+        return "done";
+    }
+
+    @GetMapping("/compuestos")
+    public List<Compuesto> getAllBooks() {
+        // The BookRepository is already injected and you can use it
+        return compuestoRepository.findAll();
+    }
+
+    @GetMapping("/compuestos/{name}")
+    public Compuesto getCompuestoByName(@PathVariable String name) {
+        Compuesto compuesto =  compuestoRepository.findById(name).get();
+        return compuesto;
+    }
+
+}
--- fda/src/main/java/com/poli/fda/repository/CompuestoRepository.java
+++ fda/src/main/java/com/poli/fda/repository/CompuestoRepository.java
@@ -2,10 +2,12 @@ package com.poli.fda.repository;
 
 import com.poli.fda.entity.Compuesto;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface CompuestoRepository extends MongoRepository<Compuesto,String> {
 
     Optional<List<Compuesto>> findCompuestosByRdaIsBetween(int min, int max);
